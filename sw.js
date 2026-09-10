42522c2b192ef346f808564b74640b1bdf8bb8ee
Alpha Picker <157329784+AlphaPicker@users.noreply.github.com>
2026-09-10 20:32:14 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('klon-v1').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon.svg'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
